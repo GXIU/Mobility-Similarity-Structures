38a9a59c350429ea89ac1395d9e16c092f171aa3
George Xiu <33057332+GXIU@users.noreply.github.com>
2024-01-22 13:21:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
 from setuptools import setup, find_packages
 import pkg_resources
+
+install_requires = [
+    'numpy',
+    'networkx',
+    'torchvision',
+    'pandas',
+]
+
+setup(
+    name='mss',
+    version='0.0.1',
+    author='Gezhi Xiu',
+    packages=['mss'],
+    install_requires=install_requires,
+)
